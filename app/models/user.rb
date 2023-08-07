8bd9f4c88d826d1e5615c47a92da8c97d1a0fae0
MauricioNC <mauricionc19@gmail.com>
2023-08-07 17:34:53 -0600

    Users controller created and added validations to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :tasks
+
+  validates :name, presence: true,
+  validates :username, presence: true, uniqueness: true,
+  validates :email, presence: true, uniqueness: true,
+  validates :password, presence: true
 end
